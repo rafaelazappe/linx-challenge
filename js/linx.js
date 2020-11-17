ecb4c94a3d7aa575a365be417f7d59f8667d7d79
rafaelazappe <64093007+rafaelazappe@users.noreply.github.com>
2020-11-17 11:55:52 -0300

    bug: tag email nos formulários

--- js/linx.js
+++ js/linx.js
@@ -74,3 +74,8 @@ function createElement(type, parent, classList) {
   parent.append(element);
   return element;
 }
+
+function checkEmail(email) {
+  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
+}
